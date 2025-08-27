9a2bb81f7949043aff3dee83e285e19501a55993
Henrye <aldairzelaya137@gmail.comm>
2025-08-26 18:58:21 -0600

    Practicas

--- practica1.js
+++ practica1.js
@@ -5,6 +5,7 @@ const rd = readline.createInterface({
     output: process.stdout
 })
 
+// ejercios 1
 // rd.question('What is your name: ',(name)=>{
 //     console.log("welcome " + name)
 //     rd.close();
@@ -18,16 +19,75 @@ const rd = readline.createInterface({
 // console.log("reverse: "+ word.split("").reverse().join(''));
 // console.log(word.replaceAll(" ","").split(','));
 
-rd.question("Ingrese una frase o una palabra: ", (word)=>{
-    let palabraInvertida = word.split('').reverse().join("");
-    console.log(palabraInvertida + " " + word)
-    let respuesta; 
-    // if(word == palabraInvertida){
-    //     respuesta = "es palindromo"
-    // }else{
-    //     respuesta = "no es palindromo"
-    // }
-    respuesta = word == palabraInvertida? "es palindromo" : "no es palindromo"
-    console.log (word + " :"+ respuesta)
+// ejercicio 2.
+// function contarVocales(cadena) {
+//     const vocales = ['a', 'e', 'i', 'o', 'u'];
+//     let contador = 0;
+
+//   cadena = cadena.toLowerCase(); // Ignorar mayúsculas
+
+//     for (let letra of cadena) {
+//     if (vocales.includes(letra)) {
+//     contador++;
+//     }
+// }
+//     return contador;
+// }
+
+// rd.question('Ingresa una frase: ', (respuesta) => {
+//     const cantidad = contarVocales(respuesta);
+//     console.log(`La frase contiene ${cantidad} vocales.`);
+//     rd.close();
+// })
+
+// Ejercicio3
+// function sonAnagramas(palabraA, palabraB) {
+//   // Normalizar: convertir a minúsculas y eliminar espacios
+//     const normalizar = (str) =>
+//     str.toLowerCase().replace(/\s+/g, '').split('').sort().join('');
+
+//     return normalizar(palabraA) === normalizar(palabraB);
+// }
+
+// rd.question('Ingresa la primera palabra: ', (palabraA) => {
+//     rd.question('Ingresa la segunda palabra: ', (palabraB) => {
+//     if (sonAnagramas(palabraA, palabraB)) {
+//     console.log('¡Son anagramas!');
+//     } else {
+//     console.log('No son anagramas.');
+//     }
+//     rd.close();
+//     });
+// });
+
+// ejercicio 4
+
+rd.question('Escribe una frase: ', (frase) => {
+  // Paso 1: Convertir todo a minúsculas y quitar signos
+    const limpio = frase.toLowerCase().replace(/[^\w\s]/g, '');
+
+  // Paso 2: Separar la frase en palabras
+    const palabras = limpio.split(' ');
+
+  // Paso 3: Contar cuántas veces aparece cada palabra
+    const conteo = {};
+    for (let palabra of palabras) {
+    if (palabra) {
+        conteo[palabra] = (conteo[palabra] || 0) + 1;
+    }
+}
+
+  // Paso 4: Buscar la palabra con más repeticiones
+    let masFrecuente = '';
+    let max = 0;
+    for (let palabra in conteo) {
+    if (conteo[palabra] > max) {
+        max = conteo[palabra];
+        masFrecuente = palabra;
+    }
+}
+
+    console.log(`La palabra más frecuente es: "${masFrecuente}"`);
     rd.close();
-})
+});
+
